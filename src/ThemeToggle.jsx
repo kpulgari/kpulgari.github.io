2ebd8dd768b37f669c6bd8d8efc2e005537d58f9
Kaushik Pulgari <kpulg2@illinois.edu>
2024-08-21 21:48:17 -0500

    Fix toggle bug

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,18 +1,24 @@
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { Sun, Moon } from "lucide-react";
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
+  const [currentTheme, setCurrentTheme] = useState(theme);
+
+  useEffect(() => {
+    setCurrentTheme(theme);
+  }, [theme]);
 
   return (
     <button
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
       className="p-2 rounded-md bg-gray-200/50 hover:bg-gray-200/75 dark:bg-gray-200/20 dark:hover:bg-gray-200/10 transition-colors duration-200 cursor-pointer"
     >
-      {theme === "dark" ? (
-        <Sun className="w-4 h-4"></Sun>
+      {currentTheme === "dark" ? (
+        <Sun className="w-4 h-4" />
       ) : (
-        <Moon className="w-4 h-4"></Moon>
+        <Moon className="w-4 h-4" />
       )}
     </button>
   );
